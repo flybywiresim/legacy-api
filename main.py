11478f493cc4d56b24eab711510b3469e2b4c91b
Will Pine <wpine215@gmail.com>
2020-10-15 22:42:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import urllib3
 from flask import Flask
 from flask import request
-from flask.ext.cache import Cache
+from flask_caching import Cache
 
 app = Flask(__name__)
 # cache = Cache(app,config={'CACHE_TYPE': 'simple'})
