2cd3b3e742e1fb3a6e32fb068bf4ce98b9085d46
Will Pine <wpine215@gmail.com>
2020-10-15 22:36:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import urllib3
 from flask import Flask
 from flask import request
-# from flask.ext.cache import Cache
+from flask.ext.cache import Cache
 
 app = Flask(__name__)
-# cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 @app.route("/")
 def index():
