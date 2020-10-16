f64e4a0267ca2990b5b64b41188472766c6c7a28
Will Pine <wpine215@gmail.com>
2020-10-15 22:37:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import request
 from flask.ext.cache import Cache
 
 app = Flask(__name__)
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+# cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 @app.route("/")
 def index():
