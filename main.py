2c23cc66b15711198cbbc7d8e25d91da2157f4f6
Will Pine <wpine215@gmail.com>
2020-10-16 02:14:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -173,9 +173,7 @@ def fetch_ivao_atis(icao):
     if not result or atis == '':
         return None
     atis_tmp = atis.split('^§')[1:]
-    atis_msg = ''.join(atis_tmp)
-    print(atis_msg)
-    atis_msg = atis_msg.replace('^§', ' ')
+    atis_msg = ' '.join(atis_tmp)
     return {"combined": atis_msg}
     
 @cache.memoize(timeout=MEMOIZE_TIMEOUT)
