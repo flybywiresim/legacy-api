09343c540917010730f2a29aacd77a83aba494c1
Will Pine <wpine215@gmail.com>
2020-10-17 01:28:09 -0400

    update sqlite path

--- main.py
+++ main.py
@@ -55,8 +55,9 @@ scheduler.start()
 ########## INITIALIZE DB ##########
 ###################################
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/fbw/api/db.sqlite' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
