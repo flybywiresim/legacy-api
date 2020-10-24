c9755b251a4df8979cec72aad093050997f00685
Niklas Steiner <niklas@sbg.at>
2020-10-24 21:48:34 +0200

    Fix DB configuration in containers

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ DEBUG = False
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////home/fbw/api/db.sqlite' 
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:////home/fbw/api/db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
